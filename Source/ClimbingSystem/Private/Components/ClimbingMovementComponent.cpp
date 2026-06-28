262bd6d4aff7d17ad1566cb8830b7599a61489f9
Santhoshlk <santhoshlukka123@gmail.com>
2026-06-28 19:42:26 +0530

    I learned added Capsule Trace Multi for Objects and to trace for climb able surfaces.

--- Source/ClimbingSystem/Private/Components/ClimbingMovementComponent.cpp
+++ Source/ClimbingSystem/Private/Components/ClimbingMovementComponent.cpp
@@ -3,3 +3,24 @@
 
 #include "Components/ClimbingMovementComponent.h"
 
+#include "Kismet/KismetSystemLibrary.h"
+
+TArray<FHitResult> UClimbingMovementComponent::CapsuleTraceMultiForObjects(const FVector& Start, const FVector& End,bool DrawDebugTypes)
+{
+   TArray<FHitResult> OutCapsuleTraceHitResult;
+	
+	UKismetSystemLibrary::CapsuleTraceMultiForObjects(
+		this,
+		Start,
+		End,
+		CapsuleTraceRadius,
+		CapsuleTraceHalfHeight,
+		ObjectTypes,
+		false,
+		TArray<AActor*>(),
+		DrawDebugTypes ? EDrawDebugTrace::ForOneFrame : EDrawDebugTrace::None,
+		OutCapsuleTraceHitResult,
+		true
+		);
+	return OutCapsuleTraceHitResult;
+}
--- Source/ClimbingSystem/Public/Components/ClimbingMovementComponent.h
+++ Source/ClimbingSystem/Public/Components/ClimbingMovementComponent.h
@@ -13,5 +13,19 @@ UCLASS()
 class CLIMBINGSYSTEM_API UClimbingMovementComponent : public UCharacterMovementComponent
 {
 	GENERATED_BODY()
+
+#pragma region Traces
+   TArray<FHitResult> CapsuleTraceMultiForObjects(const FVector& Start , const FVector& End , bool DrawDebugTypes);
 	
+#pragma endregion
+
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly,meta=(AllowPrivateAccess = "true") )
+	float CapsuleTraceRadius = 50.f;
+	
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly,meta=(AllowPrivateAccess = "true") )
+	float CapsuleTraceHalfHeight = 80.f;
+
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly,meta=(AllowPrivateAccess = "true") )
+	TArray<TEnumAsByte<EObjectTypeQuery> > ObjectTypes;
+	 
 };
